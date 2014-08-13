7a58da7b1dc28048f83413fc8829a9d59474a725
Marcelo Carvalho <eu.mcarvalho@gmail.com>
2014-08-13 15:29:51 -0300

    Characters community page has been finalized

--- app/Controller/CommunityController.php
+++ app/Controller/CommunityController.php
@@ -11,7 +11,47 @@ class CommunityController extends AppController {
 	
 	// Método characters search
 	function characters() {
-		
+		if(!empty($this->data)) { // Se o $this->data não esta vazio:
+			if(isset($this->data['name']) && !empty($this->data['name'])) { // Se estiver sendo passado $this->data['name'] e ele não estiver vazio:
+				$name = $this->data['name']; // $name recebe o valor do campo
+			} else if(isset($this->data['Player']['name']) && !empty($this->data['Player']['name'])) { // Se não, se estiver sendo passado $this->data['Player']['name'] e ele não estiver vazio:
+				$name = $this->data['Player']['name']; // $name recebe o valor do campo
+			}
+			$this->loadModel('Player'); // Carrega o Model para ser usado
+			$character = $this->Player->find( // Busca os characters relacionado a conta do usuario logado
+				'first', 
+				array(
+					'conditions' => array( // Condições de busca
+						'Player.name' => $name
+					),
+					'contain' => array( // Tabelas associadas
+						'Account'
+					),
+					'fields' => array( // Campos
+						'Player.name',
+						'Player.vocation',
+						'Player.level',
+						'Player.sex',
+						'Player.lastlogin',
+						'Player.posx',
+						'Player.posy',
+						'Player.posz',
+						'Account.premdays'
+					)
+				)
+			);
+			if(!empty($character)) { // Se não vazio o array de character:
+				$this->set('character', $character); // Seta os dados para a view
+				$vocation_player = array(); // Cria array vazio para se usar
+				foreach(Configure::read('Vocations') as $vocation_id => $vocation) { // Percorre o array de registros
+					$vocation_player[$vocation_id] = $vocation; // Cria o array para exibir na view
+				}
+				$this->set('vocation', $vocation_player); // Envia para a view os dados
+			} else { // Se não:
+				$this->Session->setFlash('Não foi possível encontrar este player!', 'default', array('class'=>'alert alert-danger')); // Retorna erro
+				//return $this->redirect($this->here); // Recarrega a página para limpar o $this->data
+			}
+		}
 	}
 	
 	// Método highscores
--- app/View/Community/characters.ctp
+++ app/View/Community/characters.ctp
@@ -2,8 +2,52 @@
 <div class="panel panel-default panel-body">
 	<?php echo $this->Form->create('',array('class' => 'form-inline')); ?>
 		<div class="form-group">
-			<?php echo $this->Form->input('name', array('label' => false, 'class' => 'form-control', 'placeholder' => 'Enter name player')); ?>
+			<?php echo $this->Form->input('name', array('label' => false, 'class' => 'form-control', 'placeholder' => 'Enter name player', 'required' => 'required')); ?>
 		</div>
 		<button type="submit" class="btn btn-default">Search</button>
 	<?php echo $this->Form->end();?>
 </div>
+<?php if(isset($character) && !empty($character)) { ?>
+<div class="panel panel-default panel-body">
+	<div class="table-responsive">
+		<table class="table">
+			<thead>
+				<tr>
+					<th colspan="2">Information player</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Name:</td>
+					<td><?php echo $character['Player']['name']; ?></td>
+				</tr>
+				<tr>
+					<td>Level:</td>
+					<td><?php echo $character['Player']['level']; ?></td>
+				</tr>
+				<tr>
+					<td>Vocation:</td>
+					<td><?php echo $vocation[$character['Player']['vocation']]; ?></td>
+				</tr>
+				<tr>
+					<td>Sex:</td>
+					<td><?php echo (($character['Player']['sex'] == 1) ? 'Male' : 'Female'); ?></td>
+				</tr>
+				<!-- <tr>
+					<td>World:</td>
+					<td><?php //echo ; ?></td>
+				</tr> -->
+				<tr>
+					<td>Last login:</td>
+					<td><?php echo ((!$character['Player']['lastlogin']) ? 'Nunca logou' : $character['Player']['lastlogin']); ?></td>
+				</tr>
+				<tr>
+					<td>Account status:</td>
+					<td><?php echo (($character['Account']['premdays'] > 0) ? 'Premium account' : 'Free account'); ?></td>
+				</tr>
+			</tbody>
+		</table
+	</div>
+	<?php //pr($character); ?>
+</div>
+<?php } ?>
