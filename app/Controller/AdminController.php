8c455ebcb7c7743419aa87f4963d6600eca0ad01
Marcelo Carvalho <eu.mcarvalho@gmail.com>
2014-08-14 14:34:53 -0300

    Dashboard has been created

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -14,7 +14,23 @@ class AdminController extends AppController {
 	// Método index
 	function index() {
 		if($this->Admin->authAdmin()) {
+			$this->loadModel('Player'); // Carrega o Model para ser usado
+			$this->loadModel('Account'); // Carrega o Model para ser usado
+			$this->loadModel('Post'); // Carrega o Model para ser usado
 			
+			$recorded = $this->Player->find('count'); // Players registrados
+			$deleted = $this->Player->find('count', array('conditions' => array('Player.deletion' => 1))); // Players deletados
+			$recordedAccounts = $this->Account->find('count'); // Accounts registradas
+			$premium = $this->Account->find('count', array('conditions' => array('Account.premdays >' => 0))); // Accounts premium
+			$free = $this->Account->find('count', array('conditions' => array('Account.premdays' => 0))); // Accounts free
+			$posts = $this->Post->find('count'); // Posts criados
+			
+			$this->set('recorded', $recorded);
+			$this->set('deleted', $deleted);
+			$this->set('recordedAccounts', $recordedAccounts);
+			$this->set('premium', $premium);
+			$this->set('free', $free);
+			$this->set('posts', $posts);
 		}
 	}
 	
--- app/View/Admin/index.ctp
+++ app/View/Admin/index.ctp
@@ -1,4 +1,32 @@
 <?php $this->set('title_for_layout', 'Admin Panel ['.$this->Session->read('Account.name').']'); // Titulo da pagina ?>
 <div class="panel panel-default panel-body">
-	<?php pr($this->params); ?>
+	<div class="col-md-4">
+		<div class="panel panel-default panel-body">
+			<fieldset>
+				<legend>Information players</legend>
+				<p>Players recorded : <?php echo $recorded; ?></p>
+				<p>Players deleted : <?php echo $deleted; ?></p>
+			</fieldset>
+		</div>
+	</div>
+	
+	<div class="col-md-4">
+		<div class="panel panel-default panel-body">
+			<fieldset>
+				<legend>Information accounts</legend>
+				<p>Accounts recorded : <?php echo $recordedAccounts; ?></p>
+				<p>Premium accounts : <?php echo $premium; ?></p>
+				<p>Free accounts : <?php echo $free; ?></p>
+			</fieldset>
+		</div>
+	</div>
+	
+	<div class="col-md-4">
+		<div class="panel panel-default panel-body">
+			<fieldset>
+				<legend>Information posts</legend>
+				<p>Posts created: <?php echo $posts; ?></p>
+			</fieldset>
+		</div>
+	</div>
 </div>
