b86269afd75a04f9652a338ee9333e55ab244f03
Marcelo Carvalho <eu.mcarvalho@gmail.com>
2014-08-14 09:36:32 -0300

    gitignore and set the configuration file of constants added to the repository

---
+++ app/Config/config.php
@@ -0,0 +1,16 @@
+<?php
+// Requerimento das configurações personalizadas definidas pelo usuário
+require_once ROOT.'/config.php';
+
+// Definição de constantes
+// Contantes da database
+define('hostname', $config['Database']['hostname']);
+define('username', $config['Database']['username']);
+define('password', $config['Database']['password']);
+define('database', $config['Database']['database']);
+// Constantes do servidor
+define('nameServer', $config['Server']['name']);
+define('titleServer', $config['Server']['title']);
+// Constante da Engine (Aplicação)
+define('layoutAAC', $config['Engine']['layout']);
+define('url',  $config['Server']['url']);
