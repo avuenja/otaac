1e9e592bc8c77706e78f954dd28b914cb7a2e9fe
Marcelo Carvalho <eu.mcarvalho@gmail.com>
2014-09-17 12:18:42 -0300

    routes changed

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -28,7 +28,7 @@
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
-	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+	Router::connect('/library/*', array('controller' => 'pages', 'action' => 'display'));
 	Router::connect('/character/*', array('controller' => 'players', 'action' => 'character')); // Router Player view
 
 /**
