c6113cb5ef235fab798cfe24f4bed8561e5f112e
Marcelo Carvalho <eu.mcarvalho@gmail.com>
2015-10-19 18:17:04 -0200

    Adjustment recommended by @renatorib

--- app/Model/Guild.php
+++ app/Model/Guild.php
@@ -40,12 +40,9 @@ class Guild extends AppModel {
 	
 	// Método beforeSave (Antes de salvar)
 	public function beforeSave($options = array()) {
-		App::import('Model', 'Player');
-		$Player = new Player();
-
 		$uid = $_SESSION['Account']['id'];
 
-		$players = $Player->find('list', array(
+		$players = $this->Player->find('list', array(
 			'conditions' => array(
 				'Player.account_id' => $uid
 			)
